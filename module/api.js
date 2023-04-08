33796928aa4d9b3b13625a1c9ff4176410bc8549
Dan Lino <danielalinodasilvaoliveira@gmail.com>
2023-04-08 03:46:57 -0300

    adicionando nova api atualizada

--- module/api.js
+++ module/api.js
@@ -2,7 +2,7 @@
 
 export const getItensCurso = async() => {
 
-    const url = `https://dull-rose-quail-yoke.cyclic.app/v1/lion-school/cursos`
+    const url = `https://perfect-moth-smock.cyclic.app/v1/lion-school/cursos`
     const response = await fetch(url)
     const data = await response.json()
 
@@ -11,7 +11,7 @@ export const getItensCurso = async() => {
 
 export const getAlunos = async(filtro) => {
 
-    const urlTodosAlunos = `https://dull-rose-quail-yoke.cyclic.app/v1/lion-school/alunos`;
+    const urlTodosAlunos = `https://perfect-moth-smock.cyclic.app/v1/lion-school/alunos`;
     const responseTodosAlunos = await fetch(urlTodosAlunos);
     const dataTodosAlunos = await responseTodosAlunos.json();
 
@@ -22,7 +22,7 @@ export const getAlunos = async(filtro) => {
 }
 
 export const getAlunosCurso = async(filtro) => {
-    const urlAlunosCurso = `https://dull-rose-quail-yoke.cyclic.app/v1/lion-school/alunos?cursos=${filtro}`
+    const urlAlunosCurso = `https://perfect-moth-smock.cyclic.app/v1/lion-school/alunos?cursos=${filtro}`
     const responseAlunosCurso = await fetch(urlAlunosCurso);
     const dataAlunosCurso = await responseAlunosCurso.json();
     console.log(dataAlunosCurso);
@@ -32,7 +32,7 @@ export const getAlunosCurso = async(filtro) => {
 }
 
 export const getAlunosStatus = async(idClicado) => {
-    const urlAlunosStatus = `https://dull-rose-quail-yoke.cyclic.app/v1/lion-school/alunos?status=${idClicado}`
+    const urlAlunosStatus = `https://perfect-moth-smock.cyclic.app/v1/lion-school/alunos?status=${idClicado}`
     const responseAlunosStatus = await fetch(urlAlunosStatus);
     const dataAlunosStatus = await responseAlunosStatus.json();
     console.log(dataAlunosStatus);
